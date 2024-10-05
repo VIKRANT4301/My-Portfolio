0c8a50d25f654f224d420725662d8493ebafae49
VIKRANT4301 <vikrantchakole36@gmail.com>
2024-10-05 20:33:58 +0530

    FILE ADDED

---
+++ script.js
@@ -0,0 +1,10 @@
+let menuList = document.getElementById("navlist")
+navlist.style.maxHeight="0px";
+
+
+     function toggleMenu() {
+        const navList = document.querySelector('.navlist');
+        navList.classList.toggle('active');
+    }
+
+
